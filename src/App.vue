b3959f57a25fcdc2a8aeb20e45cbfe420867bdfa
xuzhaowen <1716540745@qq.com>
2019-12-30 00:06:27 +0800

    测试jsonp

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,29 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import axios from "axios";
+// import axios from "axios";
+import jsonp from "jsonp";
 
 export default {
   name: "app",
   components: {
     HelloWorld
   },
+  data() {
+    return {
+      age: 30,
+      data: ""
+    };
+  },
   mounted() {
     let url = "https://m.imooc.com/api/search/searchword";
-    axios.get(url).then(() => {});
+    // 当使用axios方式请求机会报错，因为慕课网不允许别人调用，所以存在跨域
+    // axios(url).then(() => {});
+    // jsonp不会报错，因为他不是请求，就是一段js脚本,等于是函数
+    jsonp(url, (err, res) => {
+      let result = res;
+      this.data = result;
+    });
   }
 };
 </script>
