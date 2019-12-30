17343f46d2c6cb1a4f869434527aa4c8f0a9868e
xuzhaowen <1716540745@qq.com>
2019-12-30 15:02:17 +0800

    测试跨域-接口代理

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'https://www.imooc.com',
+                changeOrigin: true,
+                pathRewrite: {
+                    '/api': ''
+                }
+            }
+        }
+    }
+}
