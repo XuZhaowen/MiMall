3b38c8390952b8c179e2966dd2568ccca98dde03
xuzhaowen <1716540745@qq.com>
2020-02-02 15:22:15 +0800

    4-10  环境变量设置

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
+import env from './env'
 
 // 根据前端的跨域方式做调整 /a/b : /api/a/b =>/a/b
 axios.defaults.baseURL = '/api';
 // 超时
 axios.defaults.timeout = 8000;
+// 根據環境變量獲取不同的請求地址
+axios.defaults.baseURL = env.baseURL;
 
 // 接口错误拦截
 axios.interceptors.response.use(function (response) {
