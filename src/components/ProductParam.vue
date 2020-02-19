35f9f8fd2bb0215923990ed00b8bb20a569ececd
xuzhaowen <1716540745@qq.com>
2020-02-19 22:26:42 +0800

    8-1产品站参数组件实现

---
+++ src/components/ProductParam.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="nav-bar">
+    <div class="container">
+      <div class="product-title">
+        <span class="main-name">小米8 透明探索版</span>
+        <span class="sx">|</span>
+        <span class="little-name">小米8</span>
+        <span class="sx">|</span>
+        <span class="little-name">小米8 SE</span>
+      </div>
+      <div class="product-param">
+        <a href="javascript:;">概述</a>
+        <span>|</span>
+        <a href="javascript:;">参数</a>
+        <span>|</span>
+        <a class="user" href="javascript:;">用户评价</a>
+        <!-- slot插槽实现按钮 -->
+        <slot class="buy" name="buy"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "nav-bar"
+};
+</script>
+
+<style lang='scss'>
+@import "./../assets/scss/config.scss";
+@import "./../assets/scss/mixin.scss";
+.nav-bar {
+  height: 70px;
+  // 使文字居中
+  line-height: 70px;
+  border-top: 1px solid $colorH;
+  .container {
+    // display: flex;
+    // justify-content: space-between;
+    @include flex(space-between);
+    .product-title {
+      span {
+        margin-right: 5px;
+      }
+      .main-name {
+        font-size: $fontH;
+        color: $colorB;
+      }
+      .little-name {
+        font-size: $fontK;
+        color: $colorC;
+      }
+    }
+    .product-param {
+      span {
+        margin: 5px;
+        color: $colorD;
+      }
+      a {
+        font-size: $fontJ;
+        color: $colorC;
+        line-height: 14px;
+      }
+      .buy {
+        margin-left: 5px;
+      }
+      .user {
+        margin-right: 5px;
+      }
+    }
+  }
+}
+</style>
--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>product</div>
+  <div>
+    <ProductParams>
+      <template v-slot:buy>
+        <button class="btn">立即购买</button>
+      </template>
+    </ProductParams>
+  </div>
 </template>
 
 <script>
+import ProductParams from "./../components/ProductParam";
 export default {
-  name: "product"
+  name: "product",
+  components: {
+    ProductParams
+  }
 };
 </script>
 
