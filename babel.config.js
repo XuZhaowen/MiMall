8d33b80449bfe6007927c8370b1d5cc73352ef0f
xuzhaowen <1716540745@qq.com>
2020-03-23 13:38:50 +0800

    合并.babelrc和babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  presets: [
+  "presets": [
     '@vue/cli-plugin-babel/preset'
+  ],
+  "presets": [
+    [
+      // "es2015",
+      "@babel/preset-env",
+      {
+        "modules": false
+      }
+    ]
+  ],
+  "plugins": [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
   ]
 }
