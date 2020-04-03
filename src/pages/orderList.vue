d3036ffb525275fad57d3a301d776c120b034030
xuzhaowen <1716540745@qq.com>
2020-04-03 16:54:03 +0800

    Loading优化，订单没有数据时，NoData优化

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -8,6 +8,7 @@
     <div class="wrapper">
       <div class="container">
         <div class="order-box">
+          <loading v-if="loading"></loading>
           <div class="order" v-for="(order, index) in list" :key="index">
             <div class="order-title">
               <div class="item-info fl">
@@ -55,19 +56,25 @@
           </div>
         </div>
       </div>
+      <no-data v-if="!loading && list.length == 0"></no-data>
     </div>
   </div>
 </template>
 <script>
 import OrderHeader from "./../components/OrderHeader";
+import Loading from "./../components/Loading";
+import NoData from "./../components/NoData";
 export default {
   name: "order-list",
   components: {
-    OrderHeader
+    OrderHeader,
+    Loading,
+    NoData
   },
   data() {
     return {
-      list: []
+      list: [],
+      loading: true
     };
   },
   mounted() {
@@ -75,9 +82,15 @@ export default {
   },
   methods: {
     getOrderList() {
-      this.axios.get("/orders").then(res => {
-        this.list = res.list;
-      });
+      this.axios
+        .get("/orders")
+        .then(res => {
+          this.loading = false;
+          this.list = res.list;
+        })
+        .catch(() => {
+          this.loading = false;
+        });
     },
     goPay(orderNo) {
       // 三种路由跳转方式
