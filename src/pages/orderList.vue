0600643231ee74141e1ea6125cc4ee4915de718f
xuzhaowen <1716540745@qq.com>
2020-04-04 22:51:08 +0800

    订单列表分页，分页器

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -28,19 +28,13 @@
             </div>
             <div class="order-content clearfix">
               <div class="good-box fl">
-                <div
-                  class="good-list"
-                  v-for="(item, i) in order.orderItemVoList"
-                  :key="i"
-                >
+                <div class="good-list" v-for="(item, i) in order.orderItemVoList" :key="i">
                   <div class="good-img">
-                    <img v-lazy="item.productImage" alt="" />
+                    <img v-lazy="item.productImage" alt />
                   </div>
                   <div class="good-name">
                     <div class="p-name">{{ item.productName }}</div>
-                    <div class="p-money">
-                      {{ item.totalPrice + "X" + item.quantity }}元
-                    </div>
+                    <div class="p-money">{{ item.totalPrice + "X" + item.quantity }}元</div>
                   </div>
                 </div>
               </div>
@@ -48,14 +42,24 @@
                 <a href="javascript:;">{{ order.statusDesc }}</a>
               </div>
               <div class="good-state fr" v-else>
-                <a href="javascript:;" @click="goPay(order.orderNo)">{{
+                <a href="javascript:;" @click="goPay(order.orderNo)">
+                  {{
                   order.statusDesc
-                }}</a>
+                  }}
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <el-pagination
+        class="pagniation"
+        background
+        layout="prev, pager, next"
+        :total="total"
+        :pageSize="pageSize"
+        @current-change="handleChange"
+      ></el-pagination>
       <no-data v-if="!loading && list.length == 0"></no-data>
     </div>
   </div>
@@ -64,17 +68,24 @@
 import OrderHeader from "./../components/OrderHeader";
 import Loading from "./../components/Loading";
 import NoData from "./../components/NoData";
+// 引入分页器
+import { Pagination } from "element-ui";
+
 export default {
   name: "order-list",
   components: {
     OrderHeader,
     Loading,
-    NoData
+    NoData,
+    [Pagination.name]: Pagination
   },
   data() {
     return {
       list: [],
-      loading: true
+      loading: true,
+      pageSize: 10,
+      pageNum: 1,
+      total: 0
     };
   },
   mounted() {
@@ -83,10 +94,16 @@ export default {
   methods: {
     getOrderList() {
       this.axios
-        .get("/orders")
+        .get("/orders", {
+          params: {
+            pageNum: this.pageNum
+          }
+        })
         .then(res => {
           this.loading = false;
           this.list = res.list;
+          // 获取总共页数
+          this.total = res.total;
         })
         .catch(() => {
           this.loading = false;
@@ -108,6 +125,10 @@ export default {
           orderNo
         }
       });
+    },
+    handleChange(pageNum) {
+      this.pageNum = pageNum;
+      this.getOrderList();
     }
   }
 };
@@ -120,60 +141,68 @@ export default {
     background-color: $colorJ;
     padding-top: 33px;
     padding-bottom: 110px;
-    .order-box {
-      .order {
-        @include border();
-        background-color: $colorG;
-        margin-bottom: 31px;
-        &:last-child {
-          margin-bottom: 0;
-        }
-        .order-title {
-          @include height(74px);
-          background-color: $colorK;
-          padding: 0 43px;
-          font-size: 16px;
-          color: $colorC;
-          .item-info {
-            span {
-              margin: 0 9px;
+    .container {
+      .order-box {
+        .order {
+          @include border();
+          background-color: $colorG;
+          margin-bottom: 31px;
+          &:last-child {
+            margin-bottom: 0;
+          }
+          .order-title {
+            @include height(74px);
+            background-color: $colorK;
+            padding: 0 43px;
+            font-size: 16px;
+            color: $colorC;
+            .item-info {
+              span {
+                margin: 0 9px;
+              }
+            }
+            .money {
+              font-size: 26px;
+              color: $colorB;
             }
           }
-          .money {
-            font-size: 26px;
-            color: $colorB;
-          }
-        }
-        .order-content {
-          padding: 0 43px;
-          .good-box {
-            width: 88%;
-            .good-list {
-              display: flex;
-              align-items: center;
-              height: 145px;
-              .good-img {
-                width: 112px;
-                img {
-                  width: 100%;
+          .order-content {
+            padding: 0 43px;
+            .good-box {
+              width: 88%;
+              .good-list {
+                display: flex;
+                align-items: center;
+                height: 145px;
+                .good-img {
+                  width: 112px;
+                  img {
+                    width: 100%;
+                  }
+                }
+                .good-name {
+                  font-size: 20px;
+                  color: $colorB;
                 }
               }
-              .good-name {
-                font-size: 20px;
-                color: $colorB;
-              }
             }
-          }
-          .good-state {
-            @include height(145px);
-            font-size: 20px;
-            color: $colorA;
-            a {
+            .good-state {
+              @include height(145px);
+              font-size: 20px;
               color: $colorA;
+              a {
+                color: $colorA;
+              }
             }
           }
         }
       }
+    }
+    .pagniation {
+      text-align: center;
+    }
+    .el-pagination.is-background .el-pager li:not(.disabled).active {
+      background-color: $colorA;
     }
   }
 }
