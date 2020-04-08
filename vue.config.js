3921d1b9134f3aa206e86068102066ed89aacf89
xuzhaowen <1716540745@qq.com>
2020-04-08 13:48:34 +0800

    跨域，接口代理，备注

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,15 @@
 module.exports = {
+    // 默认加载配置表
     devServer: {
         host: 'localhost',
         port: 8080,
+        // proxy代理
         proxy: {
+            // 拦截api，代理到下面
             '/api': {
                 target: 'https://www.imooc.com',
                 changeOrigin: true,
+                // 路径转发规则
                 pathRewrite: {
                     '/api': ''
                 }
